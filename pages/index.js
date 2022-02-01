68d45b49097820a76aebb1f31a0ca81254bcbaf0
nugroho-dev <momochi@yahoo.com>
2022-02-01 13:35:49 +0700

    debug

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,60 @@ import Head from "next/head";
 import Link from "next/link";
 
 const Home = ({ posts }) => {
-  return <>test</>;
+  const [state, setState] = useContext(UserContext);
+
+  const head = () => (
+    <Head>
+      <title>MERNCAMP - A social network by devs for devs</title>
+      <meta
+        name="description"
+        content="A social network by developers for other web developers"
+      />
+      <meta
+        property="og:description"
+        content="A social network by developers for other web developers"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="MERNCAMP" />
+      <meta property="og:url" content="https://merncamp-client.vercel.app/" />
+      <meta
+        property="og:image:secure_url"
+        content="https://merncamp-client.vercel.app/images/default.jpg"
+      />
+    </Head>
+  );
+
+  return (
+    <>
+      {head()}
+      <ParallaxBG url="/images/default.jpg" />
+
+      <div className="container">
+        <div className="row pt-5">
+          {posts.map((post) => (
+            <div className="col-md-4">
+              <Link href={`/post/view/${post._id}`}>
+                <a>
+                  <PostPublic key={post._id} post={post} />
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export async function getServerSideProps() {
-  // const { data } = await axios.get("/posts");
-
-  const data = await fetch(`/posts`, {
-    method: "GET",
-  });
-
-  console.log(data);
+  const { data } = await axios.get(
+    "https://75a1-2001-448a-20a0-5fce-147-184b-dc5e-2655.ap.ngrok.io/api/posts"
+  );
+  // console.log(data);
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      posts: data,
+    },
   };
 }
 
